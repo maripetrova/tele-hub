049b44b21733c299b5b387879be58e5f6aa5143d
BuildTools <likvidoff@gmail.com>
2021-06-10 06:48:54 +0300

    heroku dev

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '15f9b854fa1438879481e2bcdc259981e328f7be',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '15f9b854fa1438879481e2bcdc259981e328f7be',
+            'dev_requirement' => false,
+        ),
+    ),
+);
