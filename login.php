d3024c9ab5f9c9911b0cc1f1f9c7fe3aae3103e5
BuildTools <likvidoff@gmail.com>
2021-06-10 07:17:59 +0300

    heroku dev

--- login.php
+++ login.php
@@ -29,7 +29,7 @@ if (isset($_POST['login']) && isset($_POST['password'])) {
 			if (password_verify($pass, $row['password'])) {
 				$_SESSION['login'] = $row['login'];
 				$_SESSION['id'] = $row['id'];
-				header("Location: profile.php");
+				header("Location: profile.html");
 				exit();
 			}else {
 				header("Location: main.php?error_signin= Неверный логин или пароль");
