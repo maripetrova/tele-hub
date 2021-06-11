06a84951d09df3d9b9a159306d86cd29d2349e14
maripetrova <masha.petrova.27012004@gmail.com>
2021-06-11 12:58:42 +0300

    Мелкие изменения

--- scripts/bot.js
+++ scripts/bot.js
@@ -50,6 +50,7 @@ bot_input.addEventListener('keydown', function (event) {
 });
 const forShowTimeoutClose = () => { bot_show.style.right = '100px' }
 const forShowTimeoutShow = () => { bot_form.style.bottom = '0px' }
+
 bot_close.addEventListener('click', function (evt) {
     bot_form.style.bottom = '-480px'
     setTimeout(forShowTimeoutClose, 500)
